9b2f5c11a3fa947db5558a0514d345850bb4d9c2
bodgur88 <57522025+bodgur88@users.noreply.github.com>
2019-11-08 13:48:31 +0530

    Add files via upload

---
+++ Mysql.java
@@ -0,0 +1,23 @@
+package mysql;
+import java.sql.*;
+
+
+/**
+ *
+ * @author HP
+ */
+public class Mysql {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","root");
+            Statement stmt = con.createStatement();
+            ResultSet rs= stmt.executeQuery("select * from emp");
+            while(rs.next())
+                System.out.println(rs.getInt(1)+" " + rs.getString(2) + " " + rs.getString(3));
+            con.close();
+        
+        }catch (Exception e) {System.out.println(e);}
+    }
+    
+}
